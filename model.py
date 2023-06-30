fe0cde8826bf1cb6c24d10ba08a41f18068a3ee7
krittaphascha <136419686+krittaphascha@users.noreply.github.com>
2023-06-30 21:37:32 +0700

    .

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import openai
 import os
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
+# from dotenv import load_dotenv, find_dotenv
+# _ = load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
